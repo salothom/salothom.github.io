8c0a980b34f06be79dfdbeab594a780833efa7ab
Sarah Thompson <sarahloisthompson@gmail.com>
2020-01-16 22:16:00 -0600

    angaulr attempt 1

---
+++ sarah-site/src/app/header/header.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['../app.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  // goHome(){
+  //   na
+  // }
+}
---
+++ sarah-site/src/app/main/home/home.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['../../app.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  // @Input() button: Button;
+
+  buttonList = ["books", "music", "tech", "contact"];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
