99dfc0918c19b2e878d8835bc4ef391ea37cb226
Sarah Thompson <sarahloisthompson@gmail.com>
2021-07-02 09:29:37 -0500

    nightlight

---
+++ mysite/src/toggle.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './toggle.css'
+
+export default function Toggle() {
+    return (
+       <>
+           <input type="checkbox" id="toggle" class=" mosthigh toggle--checkbox"/>
+          <label for="toggle" class="toggle--label mosthigh">
+            <span class="toggle--label-background"></span>
+          </label>
+       </>
+    )
+}
