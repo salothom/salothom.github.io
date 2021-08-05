1fa9a0b8aaedbba90b161ac6aca5dd187eb1335f
Sarah Thompson <sarahloisthompson@gmail.com>
2021-08-05 17:16:15 -0500

    ayyy

---
+++ src/App.js
@@ -0,0 +1,129 @@
+import logo from "./logo.svg";
+import headshot from "./photos/headshot_flowers.png";
+import headshotTwo from "./photos/dark.jpeg";
+import github from "./photos/GitHub-Mark.png";
+import linkedin from "./photos/linkedin.png";
+import devto from "./photos/devto.png";
+import Hello from "./hello";
+import Toggle from "./toggle";
+import "./App.css";
+import React, { useState } from "react";
+import MyGraph from "./MyGraph";
+
+function App() {
+  // window.onscroll = function(ev) {
+  //   if (window.scrollY > 65) {
+  //     document.getElementById("top").style.opacity = "100%";
+  //   } else {
+  //     document.getElementById("top").style.opacity = "0%";
+  //   }
+  // };
+
+  const handelShow = what => {
+    if (what == "Skills") {
+      document.getElementById("skills").style.display = "block";
+      document.getElementById("about").style.display = "none";
+      document.getElementById("portolfio").style.display = "none";
+    } else if (what == "Port") {
+      document.getElementById("skills").style.display = "none";
+      document.getElementById("about").style.display = "none";
+      document.getElementById("portolfio").style.display = "block";
+    } else if (what == "About") {
+      document.getElementById("skills").style.display = "none";
+      document.getElementById("about").style.display = "block";
+      document.getElementById("portolfio").style.display = "none";
+    }
+  };
+
+  return (
+    <>
+      <Toggle />
+      <div className="head">
+        <div className="headerTop yellowSwap"></div>
+        <div className="headershot">
+          <span className="facematch">
+            <img className="headshot sun" src={headshot} alt="headshot" />
+            <img className="headshot moon" src={headshotTwo} alt="headshot" />
+
+            <h1>Sarah Lois Thompson</h1>
+            <div className="contact-group">
+              <a target="_blank" href="https://github.com/salothom">
+                <img className="contact-links" src={github} alt="GitHub" />
+              </a>
+              <a
+                target="_blank"
+                href="https://www.linkedin.com/in/sarahthompson3/"
+              >
+                <img className="contact-links" src={linkedin} alt="Linkedin" />
+              </a>
+              <a target="_blank" href="https://dev.to/salothom">
+                <img className="contact-links" src={devto} alt="Dev.To" />
+              </a>
+              <a
+                target="_blank"
+                href="https://open.spotify.com/user/1242326189?si=nvhZn5nAQHaD48_Rt6bvGg"
+              >
+                <img
+                  className="contact-links"
+                  src="https://storage.googleapis.com/pr-newsroom-wp/1/2021/02/Spotify_Icon_RGB_Black.png"
+                  alt="Spotify"
+                />
+              </a>
+            </div>
+          </span>
+        </div>
+        <div className="headerBottom yellowSwap"></div>
+        <div className="mainContent">
+          <div className="mainButtons">
+            <div
+              onClick={() => {
+                handelShow("About");
+              }}
+              className="buttons"
+            >
+              About
+            </div>
+            <div
+              onClick={() => {
+                handelShow("Port");
+              }}
+              className="buttons"
+            >
+              Portfolio
+            </div>
+            <div
+              onClick={() => {
+                handelShow("Skills");
+              }}
+              className="buttons"
+            >
+              Skills
+            </div>
+          </div>
+          <div className=" sections">
+            <div id="about" className="about ">
+              I'm a Software Developer currently based in Austin, Texas with
+              experience developing applications using modern web development
+              techniques and frameworks. I enjoy collaberating with other
+              developers using Agile methologies, learning about web application
+              secuirty processes, and creating accessible web applications.
+            </div>
+            <div id="portolfio" className="portolfio "> Coming Soon!</div>
+            <div id="skills" className="skills "><MyGraph /> </div>
+          </div>
+        </div>
+      </div>
+
+      {/* I
+         
+              When I'm not coding, I like to do digital illistration, bake
+              bread, and listen to music. You'll sometimes find me captaining a
+              city league kickball team, facilitating a local book club, or just
+              kicking it by Barton Springs Creek. */}
+
+      {/* */}
+    </>
+  );
+}
+
+export default App;
---
+++ src/MyGraph.js
@@ -0,0 +1,104 @@
+import React from "react";
+import './graph.css';
+
+export default function MyGraph() {
+  const skills = {
+    currencies: [
+      {
+        currencyName: "JavaScript",
+        marketCap: 80
+      },
+      {
+        currencyName: "html/css",
+        marketCap: 85
+      },
+      {
+        currencyName: "Angular",
+        marketCap: 70
+      },
+      {
+        currencyName: "React",
+        marketCap: 75
+      },
+      {
+        currencyName: "Agile",
+        marketCap: 85
+      },
+      {
+        currencyName: "Swift",
+        marketCap: 60
+      }
+    ]
+  };
+
+  return (
+    <div>
+      <Graph
+        currencies={skills.currencies}
+        graphTitle="My Skills!!!!!"
+      />
+    </div>
+  );
+}
+
+class Graph extends React.Component {
+
+  renderLines() {
+    return Array(10)
+      .fill(null)
+      .map((el, i) => <Line left={i * 10} key={i} />);
+  }
+
+  renderBars() {
+    const { currencies } = this.props;
+
+    let sumOfAllCurrencies = currencies.reduce((acc, currency) => {
+      return acc + currency.marketCap;
+    }, 0);
+
+    return currencies.map(currency => {
+      const percent = currency.marketCap ;
+      return <Bar percent={percent} key={currency.currencyName} />;
+    });
+  }
+
+  render() {
+    return (
+      <div className="graph-wrapper">
+        {/* <h2> {this.props.graphTitle} </h2> */}
+
+        <div className="graph">
+          <BarTextContent currencies={this.props.currencies} />
+
+          <div className="bar-lines-container">
+            {/* {this.renderLines()} */}
+            {this.renderBars()}
+          </div>
+
+          <div style={{ width: "12%" }} />
+          {/* <Markers /> */}
+        </div>
+      </div>
+    );
+  }
+}
+
+
+
+const Bar = ({ percent }) => {
+  return <div className="bar" style={{ width: `${percent}%` }} />;
+};
+
+const BarTextContent = ({ currencies }) => {
+  return (
+    <div className="bar-text-content">
+      {currencies.map((currency,i) => (
+        <div key={i}className="text">{currency.currencyName}</div>
+      ))}
+    </div>
+  );
+};
+
+const Line = ({ left }) => {
+  return <div className="line" style={{ left: `${left}%` }} />;
+};
