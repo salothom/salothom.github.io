242dddf69b9d1b0b3d24575a3147c83e93c7dba1
Sarah Thompson <sarahloisthompson@gmail.com>
2021-09-23 09:29:28 -0500

    clean up

---
+++ src/projects.js
@@ -0,0 +1,16 @@
+import React from "react";
+import dungon from "./photos/darkmode.jpg";
+
+
+export default function Toggle(){
+    return(
+    <>
+    <img src={dungon}></img>
+    <div>Random Genre Generator</div>
+    {/* https://sarahloisthompson.com/dungon/ */}
+<div>Home library Sorting Tool</div>
+{/* //https://sarahloisthompson.com/react-site/ */}
+
+    </>
+);
+}
