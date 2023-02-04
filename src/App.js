59a5ca7885b56d691fec2706666b41cdba62401f
Sarah Thompson <sarahloisthompson@gmail.com>
2023-02-04 12:00:02 -0600

    stars

--- src/App.js
+++ src/App.js
@@ -4,47 +4,21 @@ import headshotTwo from "./photos/darkmode.jpg";
 import github from "./photos/GitHub-Mark.png";
 import linkedin from "./photos/linkedin.png";
 import devto from "./photos/devto.png";
-import Toggle from "./toggle";
 import "./App.css";
 import React, { useState } from "react";
-import MyGraph from "./MyGraph";
-
+import LibraConstellation from './librastar'
 function App() {
-  // window.onscroll = function(ev) {
-  //   if (window.scrollY > 65) {
-  //     document.getElementById("top").style.opacity = "100%";
-  //   } else {
-  //     document.getElementById("top").style.opacity = "0%";
-  //   }
-  // };
-
-  const handelShow = what => {
-    if (what === "Skills") {
-      document.getElementById("skills").style.display = "block";
-      document.getElementById("about").style.display = "none";
-      document.getElementById("portolfio").style.display = "none";
-    } else if (what === "Port") {
-      document.getElementById("skills").style.display = "none";
-      document.getElementById("about").style.display = "none";
-      document.getElementById("portolfio").style.display = "block";
-    } else if (what === "About") {
-      document.getElementById("skills").style.display = "none";
-      document.getElementById("about").style.display = "block";
-      document.getElementById("portolfio").style.display = "none";
-    }
-  };
-
   return (
     <>
-      <Toggle />
+      {/* <Toggle /> */}
       <div className="head">
-        <div className="headerTop yellowSwap"></div>
-        <div className="facematch">
-          <img className="headshot sun" src={headshot} alt="headshot" />
+        <div className="">
+        <LibraConstellation></LibraConstellation>
+        <img className="headshot sun" src={headshot} alt="headshot" />
           <img className="headshot moon" src={headshotTwo} alt="headshot" />
 
           <h1>Sarah Lois Thompson</h1>
-          <div className="contact-group">
+          <div className="">
             <a target="_blank" href="https://github.com/salothom">
               <img className="contact-links" src={github} alt="GitHub" />
             </a>
@@ -69,67 +43,21 @@ function App() {
             </a>
           </div>
         </div>
-        <div className="headerBottom yellowSwap"></div>
-        {/* <div className="mainContent">
-          <div className="mainButtons">
-            <button
-              tabindex="0"
-              onkeydown={e => {
-                console.log(e);
-                if (e.keyCode === 13) {
-                  handelShow("About");
-                }
-              }}
-              onClick={() => {
-                handelShow("About");
-              }}
-              className="buttons"
-            >
-              About
-            </button>
-            <button
-              tabindex="0"
-              onClick={() => {
-                handelShow("Port");
-              }}
-              className="buttons"
-            >
-              Portfolio
-            </button>
-            <button
-              tabindex="0"
-              onClick={() => {
-                handelShow("Skills");
-              }}
-              className="buttons"
-            >
-              Skills
-            </button>
-          </div>
-          <div className=" sections">
-            <div id="about" className="about ">
-              I'm a Software Developer currently based in Austin, Texas with
-              experience developing applications using modern web development
-              techniques and frameworks. I enjoy collaberating with other
-              developers using Agile methologies, learning about web application
-              secuirty processes, and creating accessible web applications.
-            </div>
-            <div id="portolfio" className="portolfio ">
-              Coming Soon!
-            </div>
-            <div id="skills" className="skills ">
-              <MyGraph />
-            </div>
-          </div> */}
-        {/* </div> */}
+        <div>
+          When I'm not coding, I like to do digital illistration, bake bread,
+          and listen to music. You'll sometimes find me captaining a city league
+          kickball team, facilitating a local book club, or just kicking it by
+          Barton Springs Creek.
+        </div>
+        <h2>Look</h2> <h2>Look</h2>
+        <h2>Look</h2>
+        <h2>Look</h2>
+        <h2>Look</h2>
+        <h2>Look</h2>
+        <h2>Look</h2>
+        <h2>Look</h2>
+        <h2>Look</h2>
       </div>
-
-      {/* I
-         
-              When I'm not coding, I like to do digital illistration, bake
-              bread, and listen to music. You'll sometimes find me captaining a
-              city league kickball team, facilitating a local book club, or just
-              kicking it by Barton Springs Creek. */}
     </>
   );
 }
---
+++ src/librastar.js
@@ -0,0 +1,46 @@
+import "./librastar.css";
+export default function LibraConstellation() {
+  return (
+    <div class="night-sky">
+      <div class="star-box">
+        <div class="star twinkle-star-6"></div>
+        <div class="star twinkle-star-7"></div>
+        <div class="star twinkle-star-8"></div>
+        <div class="star twinkle-star-9"></div>
+        <div class="star twinkle-star-10"></div>
+        <div class="star twinkle-star-11"></div>
+        <div class="star twinkle-star-12"></div>
+        <div class="star twinkle-star-12"></div>
+
+      </div>
+      <div class="star-box">
+        <div class="star twinkle-star-1"></div>
+        <div class="star twinkle-star-2"></div>
+        <div class="star twinkle-star-3"></div>
+        <div class="star twinkle-star-4"></div>
+        <div class="star twinkle-star-5"></div>
+        <div class="star twinkle-star-6"></div>
+        <div class="star twinkle-star-7"></div>
+        <div class="star twinkle-star-9"></div>
+        <div class="star twinkle-star-10"></div>
+        {/* <div class="line line-1"></div>
+        <div class="line line-2"></div>
+        <div class="line line-3"></div>
+        <div class="line line-4"></div>
+        <div class="line line-5"></div>
+        <div class="line line-6"></div>
+        <div class="line line-7"></div> */}
+      </div>
+      <div class="star-box">
+        <div class="star twinkle-star-6"></div>
+        <div class="star twinkle-star-7"></div>
+        <div class="star twinkle-star-8"></div>
+        <div class="star twinkle-star-9"></div>
+        <div class="star twinkle-star-10"></div>
+        <div class="star twinkle-star-11"></div>
+        <div class="star twinkle-star-12"></div>
+        <div class="star twinkle-star-12"></div>
+      </div>
+    </div>
+  );
+}
