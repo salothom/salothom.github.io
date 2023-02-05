3cf50c3a00444441eb768b02e155edb7702515ed
Sarah Thompson <sarahloisthompson@gmail.com>
2023-02-04 21:02:21 -0600

    stars

--- src/App.js
+++ src/App.js
@@ -6,50 +6,24 @@ import linkedin from "./photos/linkedin.png";
 import devto from "./photos/devto.png";
 import "./App.css";
 import React, { useState } from "react";
-import LibraConstellation from './librastar'
+import LibraConstellation from "./librastar";
 function App() {
   return (
     <>
       {/* <Toggle /> */}
       <div className="head">
         <div className="">
-        <LibraConstellation></LibraConstellation>
-        <img className="headshot sun" src={headshot} alt="headshot" />
-          <img className="headshot moon" src={headshotTwo} alt="headshot" />
-
-          <h1>Sarah Lois Thompson</h1>
-          <div className="">
-            <a target="_blank" href="https://github.com/salothom">
-              <img className="contact-links" src={github} alt="GitHub" />
-            </a>
-            <a
-              target="_blank"
-              href="https://www.linkedin.com/in/sarahthompson3/"
-            >
-              <img className="contact-links" src={linkedin} alt="Linkedin" />
-            </a>
-            <a target="_blank" href="https://dev.to/salothom">
-              <img className="contact-links" src={devto} alt="Dev.To" />
-            </a>
-            <a
-              target="_blank"
-              href="https://open.spotify.com/user/1242326189?si=nvhZn5nAQHaD48_Rt6bvGg"
-            >
-              <img
-                className="contact-links"
-                src="https://storage.googleapis.com/pr-newsroom-wp/1/2021/02/Spotify_Icon_RGB_Black.png"
-                alt="Spotify"
-              />
-            </a>
-          </div>
+          <h1 className={"mainName"}>Sarah Lois Thompson</h1>
+          <LibraConstellation></LibraConstellation>
+          <img className="headshot " src={headshotTwo} alt="headshot" />
         </div>
-        <div>
+        <div className="paragraph">
           When I'm not coding, I like to do digital illistration, bake bread,
           and listen to music. You'll sometimes find me captaining a city league
-          kickball team, facilitating a local book club, or just kicking it by
-          Barton Springs Creek.
+          kickball team, takinga pottery class, or just kicking it by Barton
+          Springs.
         </div>
-        <h2>Look</h2> <h2>Look</h2>
+        <h2>Look</h2> 
         <h2>Look</h2>
         <h2>Look</h2>
         <h2>Look</h2>
@@ -57,6 +31,28 @@ function App() {
         <h2>Look</h2>
         <h2>Look</h2>
         <h2>Look</h2>
+        <h2>Look</h2>
+        <div className="contact">
+          <a target="_blank" href="https://github.com/salothom">
+            <img className="contact-links" src={github} alt="GitHub" />
+          </a>
+          <a target="_blank" href="https://www.linkedin.com/in/sarahthompson3/">
+            <img className="contact-links" src={linkedin} alt="Linkedin" />
+          </a>
+          <a target="_blank" href="https://dev.to/salothom">
+            <img className="contact-links" src={devto} alt="Dev.To" />
+          </a>
+          <a
+            target="_blank"
+            href="https://open.spotify.com/user/1242326189?si=nvhZn5nAQHaD48_Rt6bvGg"
+          >
+            <img
+              className="contact-links"
+              src="https://storage.googleapis.com/pr-newsroom-wp/1/2021/02/Spotify_Icon_RGB_Black.png"
+              alt="Spotify"
+            />
+          </a>
+        </div>
       </div>
     </>
   );
--- src/librastar.js
+++ src/librastar.js
@@ -3,7 +3,7 @@ export default function LibraConstellation() {
   return (
     <div class="night-sky">
       <div class="star-box">
-        <div class="star twinkle-star-6"></div>
+        <div class="star twinkle-star-1"></div>
         <div class="star twinkle-star-7"></div>
         <div class="star twinkle-star-8"></div>
         <div class="star twinkle-star-9"></div>
@@ -11,7 +11,7 @@ export default function LibraConstellation() {
         <div class="star twinkle-star-11"></div>
         <div class="star twinkle-star-12"></div>
         <div class="star twinkle-star-12"></div>
-
+        <div class="star twinkle-star-13"></div>
       </div>
       <div class="star-box">
         <div class="star twinkle-star-1"></div>
@@ -23,13 +23,13 @@ export default function LibraConstellation() {
         <div class="star twinkle-star-7"></div>
         <div class="star twinkle-star-9"></div>
         <div class="star twinkle-star-10"></div>
-        {/* <div class="line line-1"></div>
+        <div class="line line-1"></div>
         <div class="line line-2"></div>
         <div class="line line-3"></div>
         <div class="line line-4"></div>
         <div class="line line-5"></div>
         <div class="line line-6"></div>
-        <div class="line line-7"></div> */}
+        <div class="line line-7"></div>
       </div>
       <div class="star-box">
         <div class="star twinkle-star-6"></div>
